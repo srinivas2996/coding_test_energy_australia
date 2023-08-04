4f767f04893d15f89cde4aec369c2ebf0e900ac6
Srinivas_V <srinivas2096@outlook.com>
2023-08-04 14:03:28 +0530

    Update test_data_quality.py

--- test_data_quality.py
+++ test_data_quality.py
@@ -1,33 +1,20 @@
+# test_expectations.py
 import pytest
 import great_expectations as ge
 
-@pytest.fixture(scope="module")
-def data_batch():
-    # Load the CSV file as a batch
-    batch_kwargs = {
-        "path": r"C:\Users\User\Documents\cust-test.csv",  # Replace with the actual path to your CSV file
-        "datasource": "files",
-    }
-    batch = ge.data_context.DataContext().get_batch(batch_kwargs=batch_kwargs)
-    return batch
+@pytest.fixture
+def read_csv_file():
+    # Load the CSV file using great_expectations
+    return ge.read_csv(r"C:\Users\User\Documents\cust-test.csv")
 
-def test_data_expectations(data_batch):
-    # Load an existing expectation suite or create a new one if it doesn't exist
-    try:
-        expectation_suite = ge.data_context().get_expectation_suite("my_suite")
-    except ge.exceptions.exceptions.DataContextError:
-        expectation_suite = ge.data_context().suite_edit("my_suite")
+def test_expect_column_values_not_to_be_null(read_csv_file):
+    result = read_csv_file.expect_column_values_to_not_be_null("customerid")
+    assert result.success, f"Column 'customer_id' has null values"
 
-    # Define expectations fluently and add them to the suite
-    expectation_suite.expect_column_values_to_not_be_null("customerid")
-    expectation_suite.expect_column_values_to_be_between("age", min_value=18, max_value=120)
-    expectation_suite.expect_column_values_to_be_in_set("gender", ["Male", "Female", "Other"])
+def test_expect_column_values_to_be_between(read_csv_file):
+    result = read_csv_file.expect_column_values_to_be_between("age", min_value=10, max_value=100)
+    assert result.success, f"Column 'age' has values outside the specified range"
 
-    # Save the expectation suite
-    expectation_suite.save()
-
-    # Validate the expectation suite against the data batch
-    results = data_batch.validate(expectation_suite)
-
-    # Check if the expectations are met
-    assert results["success"], "Data expectations not met!"
+def test_expect_column_values_to_be_in_set(read_csv_file):
+    result = read_csv_file.expect_column_values_to_be_in_set("gender", ["M", "F", "Other"])
+    assert result.success, f"Column 'gender' has values not in the specified set"
